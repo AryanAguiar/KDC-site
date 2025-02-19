e61e692aaf8a780ca71febe3996c9e3d83685712
Aryan Aguiar <aguiararyan@gmail.com>
2025-02-19 18:14:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import Layout from './components/Layouts';
 import Companies from './pages/Companies'
 import Industries from './pages/Industries';
 import Portfolio from './pages/Portfolio';
-import Contactus from './pages/Contactus'; 
+import Contactus from './pages/Contactus';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
   return (
+    <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home/>} />
-          <Route path='/services' element={<Services/>} />
-          <Route path='/company' element={<Companies/>} />
-          <Route path='/industries' element={<Industries/>} />
-          <Route path='/portfolio' element={<Portfolio/>} />
-          <Route path='/contact' element={<Contactus/>} />
+          <Route index element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/company' element={<Companies />} />
+          <Route path='/industries' element={<Industries />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/contact' element={<Contactus />} />
         </Route>
       </Routes>
+      <ScrollToTop />
+    </>
   );
 };
 
